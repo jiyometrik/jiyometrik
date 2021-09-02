afe11b0bb653511cc46f838a90ba81bcda64a041
g-e-o-m-e-t-r-i-c <darrenyap08@gmail.com>
2021-09-02 12:12:39 +0800

    removed syntax highlighting due to compatibility issues between terminals:

--- dotfiles-wsl/config.fish
+++ dotfiles-wsl/config.fish
@@ -1,8 +1,5 @@
-# syntax highlighting
-set fish_color_normal brcyan
-set fish_color_command brcyan
-set fish_color_error "#ea6962"
-set fish_color_param "#7daea3"
+# config.fish - add this to the ~/.config/fish directory
+
 # enable vim mode
 set -U fish_key_bindings fish_vi_key_bindings
 
