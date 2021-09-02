4b663f33dbf67150a243f8c1e31b91206c943058
Darren Yap <darrenyap08@gmail.com>
2021-09-02 12:05:04 +0800

    configured syntax highlighting for fish

--- dotfiles-wsl/config.fish
+++ dotfiles-wsl/config.fish
@@ -1,5 +1,13 @@
+# syntax highlighting
+set fish_color_normal brcyan
+set fish_color_command brcyan
+set fish_color_error "#ea6962"
+set fish_color_param "#7daea3"
+# enable vim mode
+set -U fish_key_bindings fish_vi_key_bindings
+
 # alias for neovim
 alias nv="nvim"
-set -U fish_key_bindings fish_vi_key_bindings
+
 # starship prompt - see starship.toml
 starship init fish | source
